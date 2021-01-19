b0534d5035ea0f5c478f5b2ccc6bfedb9d983662
Matt Brown <matthewbrown0798@gmail.com>
2021-01-19 15:22:40 -0500

    added sent images to firebase

--- snapclone/src/Preview.js
+++ snapclone/src/Preview.js
@@ -3,7 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import "./Preview.css";
 import { resetCameraImage, selectCameraImage } from "./features/cameraSlice";
-
+import { v4 as uuid } from "uuid";
+import firebase from "firebase";
 import {
   TextFields,
   Close,
@@ -15,11 +16,45 @@ import {
   Timer,
   Send,
 } from "@material-ui/icons";
+import { db, storage } from "./firebase";
 
 const Preview = () => {
   const cameraImage = useSelector(selectCameraImage);
   const history = useHistory();
   const dispatch = useDispatch();
+
+  const sendPost = () => {
+    const id = uuid();
+    const uploadTask = storage
+      .ref(`posts/${id}`)
+      .putString(cameraImage, "data_url");
+    // loading func, error func, success func
+    uploadTask.on(
+      "state_changed",
+      null,
+      (err) => {
+        console.log(err);
+      },
+      () => {
+        // on complete
+        storage
+          .ref("posts")
+          .child(id)
+          .getDownloadURL()
+          .then((url) => {
+            db.collection("posts").add({
+              imageUrl: url,
+              username: "Matt",
+              read: false,
+              //   this is where I would add time to view remaining
+              //   profilePic
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            });
+            history.replace("/chats");
+          });
+      }
+    );
+  };
 
   useEffect(() => {
     if (!cameraImage) {
@@ -45,7 +80,7 @@ const Preview = () => {
         <Timer />
       </div>
       <img src={cameraImage} alt="preview" />
-      <div className="preview__footer">
+      <div className="preview__footer" onClick={sendPost}>
         <h2>Send Now</h2>
         <Send className="preview__sendIcon" />
       </div>
--- snapclone/src/firebase.js
+++ snapclone/src/firebase.js
@@ -15,7 +15,7 @@ const db = firebaseApp.firestore();
 
 const auth = firebase.auth();
 
-const storage = firebase.storage;
+const storage = firebase.storage();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
