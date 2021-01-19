0dd704bb486fbcc8fcdd50fd8438947454d698f2
Matt Brown <matthewbrown0798@gmail.com>
2021-01-19 14:24:44 -0500

    grabbing image for redux and displaying in preview screen

--- snapclone/src/Preview.js
+++ snapclone/src/Preview.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./Preview.css";
+import { selectCameraImage } from "./features/cameraSlice";
 
 const Preview = () => {
-  return <div></div>;
+  const cameraImage = useSelector(selectCameraImage);
+  return (
+    <div className="preview">
+      <h2>This is the preview</h2>
+      <img src={cameraImage} alt="preview" />
+    </div>
+  );
 };
 
 export default Preview;
