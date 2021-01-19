a4bcf780ebf9638edf524ababd9d251aa3105a51
Matt Brown <matthewbrown0798@gmail.com>
2021-01-19 14:28:38 -0500

    redirect to home page if no image in redux

--- snapclone/src/Preview.js
+++ snapclone/src/Preview.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import "./Preview.css";
 import { selectCameraImage } from "./features/cameraSlice";
 
 const Preview = () => {
   const cameraImage = useSelector(selectCameraImage);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!cameraImage) {
+      history.replace("/");
+    }
+  }, [cameraImage, history]);
+
   return (
     <div className="preview">
       <h2>This is the preview</h2>
