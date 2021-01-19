b94199c4bb071d5e2a2248ccbd77a410a3b22865
Matt Brown <matthewbrown0798@gmail.com>
2021-01-19 16:55:22 -0500

    mark messgae as read when opened

--- snapclone/src/Chat.js
+++ snapclone/src/Chat.js
@@ -3,10 +3,28 @@ import { Avatar } from "@material-ui/core";
 import { StopRounded } from "@material-ui/icons";
 import ReactTimeago from "react-timeago";
 import "./Chat.css";
+import { useHistory } from "react-router-dom";
+import { selectImage } from "./features/appSlice";
+import { useDispatch } from "react-redux";
+import { db } from "./firebase";
 
 function Chat({ id, username, timestamp, read, imageUrl, profilePic }) {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const open = () => {
+    if (!read) {
+      dispatch(selectImage(imageUrl));
+      db.collection("posts").doc(id).set(
+        {
+          read: true,
+        },
+        { merge: true }
+      );
+      history.push("/chats/view");
+    }
+  };
   return (
-    <div className="chat">
+    <div onClick={open} className="chat">
       <Avatar src={profilePic} className="chat__avatar" />
       <div className="chat__info">
         <h4>{username}</h4>
