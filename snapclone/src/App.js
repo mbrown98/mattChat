edf8c1d8d479a753f6b8645f7c2aab0656931c22
Matt Brown <matthewbrown0798@gmail.com>
2021-01-19 19:05:25 -0500

    added firebase login

--- snapclone/src/App.js
+++ snapclone/src/App.js
@@ -6,25 +6,37 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Preview from "./Preview";
 import Chats from "./Chats";
 import ChatView from "./ChatView";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "./features/appSlice";
+import Login from "./Login";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
   return (
     <div className="app">
-      <Router className="app__body">
-        <Switch>
-          <Route path="/" exact>
-            <WebcamCapture />
-          </Route>
-          <Route path="/chats/view">
-            <ChatView />
-          </Route>
-          <Route path="/preview">
-            <Preview />
-          </Route>
-          <Route path="/chats">
-            <Chats />
-          </Route>
-        </Switch>
+      <Router>
+        {!user ? (
+          <Login />
+        ) : (
+          <div className="app__body">
+            <Switch>
+              <Route path="/" exact>
+                <WebcamCapture />
+              </Route>
+              <Route path="/chats/view">
+                <ChatView />
+              </Route>
+              <Route path="/preview">
+                <Preview />
+              </Route>
+              <Route path="/chats">
+                <Chats />
+              </Route>
+            </Switch>
+          </div>
+        )}
       </Router>
     </div>
   );
---
+++ snapclone/src/Login.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { auth, provider } from "./firebase";
+import "./Login.css";
+import { login } from "./features/appSlice";
+
+const Login = () => {
+  const dispatch = useDispatch();
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((res) => {
+        console.log(res);
+        dispatch(
+          login({
+            username: res.user.displayName,
+            profilePic: res.user.photoUrl,
+            id: res.user.uid,
+          })
+        );
+      })
+      .catch((err) => alert(err.message));
+  };
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://scx2.b-cdn.net/gfx/news/2017/1-snapchat.jpg"
+          alt="snapLogo"
+        />
+        <Button variant="outlined" onClick={signIn}>
+          Sign In
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
